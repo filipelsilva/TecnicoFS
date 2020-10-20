e25213707bf3d01b012beccf60984090a881e474
Filipe Ligeiro Silva <filipe.ligeiro.silva@hotmail.com>
2020-10-20 16:49:30 +0100

    made runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Number of arguments check
 if [ $# -ne 3 ] ; then
@@ -10,14 +10,23 @@ inputdir=$1
 outputdir=$2
 maxthreads=$3
 
+filter="TecnicoFS completed in"
+
 # Make and check for errors
 if ! make > /dev/null; then
 	echo "Error: Compilation failed"
 	exit 1
 fi
 
-for file in `ls $1`; do
-	echo $file
+# Create the output directory, in case it doesn't exist
+mkdir -p $outputdir
+
+# Run the program
+for file in `ls -p $inputdir | grep -v /`; do
+	for (( numthreads=1; numthreads<=$maxthreads; numthreads++ )); do
+		echo "InputFile=$file NumThreads=$numthreads"
+		./tecnicofs "$inputdir/$file" "$outputdir/$file-$numthreads" $numthreads mutex | grep "$filter"
+	done
 done
 
 # Make clean and check for errors
