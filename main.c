a6a5961d2433d543caef1b32dd77e0016b69c0d9
Sofia-Morgado <Sofia-Morgado@github.com>
2020-11-03 16:10:38 +0000

    changing some identifi

--- main.c
+++ main.c
@@ -100,21 +100,19 @@ char* removeCommand() {
 		
 	/* lock acess to call vector */
 		
-		while (numberCommands == 0) 
-			pthread_cond_wait(&canTake,&call_vector);
+	while (numberCommands == 0) 
+		pthread_cond_wait(&canTake,&call_vector);
 
-		command = inputCommands[itake++];
+	command = inputCommands[itake++];
 
-		if (itake == MAX_COMMANDS) 
-			itake = 0;
+	if (itake == MAX_COMMANDS) 
+		itake = 0;
 
-		pthread_cond_signal(&canPut);
-        numberCommands--;
-        
-		call_vector_unlock();
-		return command; 
-
-
+	pthread_cond_signal(&canPut);
+	numberCommands--;
+	
+	call_vector_unlock();
+	return command; 
 }
 
 void errorParse() {
@@ -173,63 +171,61 @@ void processInput() {
 
 void applyCommands() {
    	while (1) {
-		   if(numberCommands > 0){
-		
-		
-			const char* command = removeCommand();
+	
+	if(numberCommands > 0){
 
-		
+	const char* command = removeCommand();
 
-			if (command == NULL) {
-				continue;
-			}
+	if (command == NULL) {
+		continue;
+	}
 
-			char token, type;
-			char name[MAX_INPUT_SIZE];
-			int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-			if (numTokens < 2) {
-				fprintf(stderr, "Error: invalid command in Queue\n");
-				exit(EXIT_FAILURE);
-			}
+	char token, type;
+	char name[MAX_INPUT_SIZE];
+	int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+	if (numTokens < 2) {
+		fprintf(stderr, "Error: invalid command in Queue\n");
+		exit(EXIT_FAILURE);
+	}
 
-			int searchResult;
-			switch (token) {
-				case 'c':
-					switch (type) {
-						case 'f':
-							printf("Create file: %s\n", name);
-							create(name, T_FILE);
-							break;
-						case 'd':
-							printf("Create directory: %s\n", name);
-							create(name, T_DIRECTORY);
-							break;
-						default:
-							fprintf(stderr, "Error: invalid node type\n");
-							exit(EXIT_FAILURE);
-					}
+	int searchResult;
+	switch (token) {
+		case 'c':
+			switch (type) {
+				case 'f':
+					printf("Create file: %s\n", name);
+					create(name, T_FILE);
 					break;
-
-				case 'l': 
-					searchResult = lookup(name);
-					if (searchResult >= 0)
-						printf("Search: %s found\n", name);
-					else
-						printf("Search: %s not found\n", name);
-					
-					break;
-
 				case 'd':
-					printf("Delete: %s\n", name);
-					delete(name);
+					printf("Create directory: %s\n", name);
+					create(name, T_DIRECTORY);
 					break;
-
-				default: { /* error */
-					fprintf(stderr, "Error: command to apply\n");
+				default:
+					fprintf(stderr, "Error: invalid node type\n");
 					exit(EXIT_FAILURE);
-				}
-			} 
-		   }
+			}
+			break;
+
+		case 'l': 
+			searchResult = lookup(name);
+			if (searchResult >= 0)
+				printf("Search: %s found\n", name);
+			else
+				printf("Search: %s not found\n", name);
+			
+			break;
+
+		case 'd':
+			printf("Delete: %s\n", name);
+			delete(name);
+			break;
+
+		default: { /* error */
+			fprintf(stderr, "Error: command to apply\n");
+			exit(EXIT_FAILURE);
+		}
+	} 
+	}
 	}
 }
 
@@ -249,15 +245,13 @@ void* fnThread_consumer() {
 /* process pool initializer and runner */
 void processPool() {
 	int i = 0;
-	pthread_t tid_producer[numberThreads];
-	pthread_t tid_consumer[numberThreads];
+	pthread_t tid_producer;
+	pthread_t tid_consumer[numberThreads - 1];
    	 
 	
-    for (i = 0; i < numberThreads; i++) {
-        if (pthread_create(&tid_producer[i], NULL, fnThread_producer, NULL)) {
+    if (pthread_create(&tid_producer, NULL, fnThread_producer, NULL)){
             fprintf(stderr, "Error: could not create threads\n");
             exit(EXIT_FAILURE);
-        }
     }
 
 	for (i = 0; i < numberThreads; i++) {
@@ -268,11 +262,9 @@ void processPool() {
     }
 	
 
-    for (i = 0; i < numberThreads; i++) {
-		if (pthread_join(tid_producer[i], NULL)) {
-			fprintf(stderr, "Error: could not join thread\n");
-		}
-    }
+    if (pthread_join(tid_producer[i], NULL)) {
+		fprintf(stderr, "Error: could not join thread\n");
+	}
 
 	for (i = 0; i < numberThreads; i++) {
 		if (pthread_join(tid_consumer[i], NULL)) {
