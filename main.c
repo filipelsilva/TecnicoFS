7a31a4166330e70bf010177819b078622c1bab50
Sofia-Morgado <Sofia-Morgado@github.com>
2020-11-03 16:19:51 +0000

    chaging some details in the project

--- main.c
+++ main.c
@@ -141,79 +141,71 @@ void call_vector_unlock() {
 }
 
 
-void applyCommands() {
+void* applyCommands() {
    	while (1) {
 		/* lock acess to call vector */
 		call_vector_lock();
-		
-		if (numberCommands > 0) {
 
-			const char* command = removeCommand();
-
-			/* unlock acess to call vector */
-			call_vector_unlock();
-
-			if (command == NULL) {
-				continue;
-			}
-
-			char token, type;
-			char name[MAX_INPUT_SIZE];
-			int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-			if (numTokens < 2) {
-				fprintf(stderr, "Error: invalid command in Queue\n");
-				exit(EXIT_FAILURE);
-			}
-
-			int searchResult;
-			switch (token) {
-				case 'c':
-					switch (type) {
-						case 'f':
-							printf("Create file: %s\n", name);
-							create(name, T_FILE);
-							break;
-						case 'd':
-							printf("Create directory: %s\n", name);
-							create(name, T_DIRECTORY);
-							break;
-						default:
-							fprintf(stderr, "Error: invalid node type\n");
-							exit(EXIT_FAILURE);
-					}
-					break;
-
-				case 'l': 
-					searchResult = lookup(name);
-					if (searchResult >= 0)
-						printf("Search: %s found\n", name);
-					else
-						printf("Search: %s not found\n", name);
-					
-					break;
-
-				case 'd':
-					printf("Delete: %s\n", name);
-					delete(name);
-					break;
-
-				default: { /* error */
-					fprintf(stderr, "Error: command to apply\n");
-					exit(EXIT_FAILURE);
-				}
-			} 
-    	}
-
-		else {
+		if (numberCommands <= 0) {
 			call_vector_unlock();
 			break;
 		}
-	}
-}
 
-/* wrapper function, calling applyCommands */
-void* fnThread() {
-	applyCommands();
+		const char* command = removeCommand();
+
+		/* unlock acess to call vector */
+		call_vector_unlock();
+
+		if (command == NULL) {
+			continue;
+		}
+
+		char token, type;
+		char name[MAX_INPUT_SIZE];
+		int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+		if (numTokens < 2) {
+			fprintf(stderr, "Error: invalid command in Queue\n");
+			exit(EXIT_FAILURE);
+		}
+
+		int searchResult;
+		switch (token) {
+			case 'c':
+				switch (type) {
+					case 'f':
+						printf("Create file: %s\n", name);
+						create(name, T_FILE);
+						break;
+					case 'd':
+						printf("Create directory: %s\n", name);
+						create(name, T_DIRECTORY);
+						break;
+					default:
+						fprintf(stderr, "Error: invalid node type\n");
+						exit(EXIT_FAILURE);
+				}
+				break;
+
+			case 'l': 
+				searchResult = lookup(name);
+				if (searchResult >= 0)
+					printf("Search: %s found\n", name);
+				else
+					printf("Search: %s not found\n", name);
+				
+				break;
+
+			case 'd':
+				printf("Delete: %s\n", name);
+				delete(name);
+				break;
+
+			default: { /* error */
+				fprintf(stderr, "Error: command to apply\n");
+				exit(EXIT_FAILURE);
+			}
+		}	
+	}
 	return NULL;
 }
 
@@ -223,7 +215,7 @@ void processPool() {
 	pthread_t tid[numberThreads];
    	 
     for (i = 0; i < numberThreads; i++) {
-        if (pthread_create(&tid[i], NULL, fnThread, NULL)) {
+        if (pthread_create(&tid[i], NULL, applyCommands, NULL)) {
             fprintf(stderr, "Error: could not create threads\n");
             exit(EXIT_FAILURE);
         }
@@ -233,6 +225,7 @@ void processPool() {
     for (i = 0; i < numberThreads; i++) {
 		if (pthread_join(tid[i], NULL)) {
 			fprintf(stderr, "Error: could not join thread\n");
+			exit(EXIT_FAILURE);
 		}
     }
 
