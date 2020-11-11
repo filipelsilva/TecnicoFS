03c7c1bb4d75f2bbd5355e4969aa496635ac1a12
Sofia-Morgado <Sofia-Morgado@github.com>
2020-11-11 22:18:00 +0000

    creating move function

---
+++ cmake-build-debug/CMakeFiles/TecnicoFS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TecnicoFS.dir/fs/operations.c.o"
+  "CMakeFiles/TecnicoFS.dir/fs/state.c.o"
+  "CMakeFiles/TecnicoFS.dir/main.c.o"
+  "TecnicoFS"
+  "TecnicoFS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TecnicoFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
