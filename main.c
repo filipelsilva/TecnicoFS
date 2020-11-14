906601515f0e11b1508ad3d53ab93faf455b0bfa
Filipe Ligeiro Silva <filipe.ligeiro.silva@hotmail.com>
2020-11-14 18:01:04 +0000

    Revert "merge"
    This reverts commit 4033b8ff7473e683fca12055992f27fb65e67c01, reversing
    changes made to 23cfc09b7246e6e946cc89b1d3ae92ce543a9940.

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ FILE* openFile(char* name, char* mode) {
 	FILE* fp = fopen(name, mode);
 	
 	if (fp == NULL) {	
-		fprintf(stderr, "Error: Error: could not open file\n");
+		fprintf(stderr, "Error: could not open file\n");
     	exit(TECNICOFS_ERROR_FILE_NOT_FOUND);
 	}
 	
@@ -84,9 +84,10 @@ void processInput(FILE *file) {
 
     /* break loop with ^Z or ^D */
     while (fgets(line, sizeof(line)/sizeof(char), file)) {
-        char token;
-        char name[MAX_INPUT_SIZE], type[MAX_INPUT_SIZE];
-        int numTokens = sscanf(line, "%c %s %s", &token, name, type);
+        char token, type;
+        char name[MAX_INPUT_SIZE];
+
+        int numTokens = sscanf(line, "%c %s %c", &token, name, &type);
 
         /* perform minimal validation */
         if (numTokens < 1) {
@@ -113,14 +114,7 @@ void processInput(FILE *file) {
                 if(insertCommand(line))
                     break;
                 return;
-			
-			case 'm':
-				if(numTokens != 3)
-					errorParse();
-				if(insertCommand(line))
-                    break;
-                return;
-
+            
             case '#':
                 break;
             
@@ -147,87 +141,88 @@ void call_vector_unlock() {
 }
 
 
-void* applyCommands() {
+void applyCommands() {
    	while (1) {
 		/* lock acess to call vector */
 		call_vector_lock();
 		
-		if (numberCommands <= 0) {
+		if (numberCommands > 0) {
+
+			const char* command = removeCommand();
+
+			/* unlock acess to call vector */
+			call_vector_unlock();
+
+			if (command == NULL) {
+				continue;
+			}
+
+			char token, type;
+			char name[MAX_INPUT_SIZE];
+			int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+			if (numTokens < 2) {
+				fprintf(stderr, "Error: invalid command in Queue\n");
+				exit(EXIT_FAILURE);
+			}
+
+			int searchResult;
+			switch (token) {
+				case 'c':
+					switch (type) {
+						case 'f':
+							//printf("Create file: %s\n", name);
+							create(name, T_FILE);
+							break;
+						case 'd':
+							//printf("Create directory: %s\n", name);
+							create(name, T_DIRECTORY);
+							break;
+						default:
+							fprintf(stderr, "Error: invalid node type\n");
+							exit(EXIT_FAILURE);
+					}
+					break;
+
+				case 'l': 
+					searchResult = lookup(name);
+					if (searchResult >= 0)
+						printf("Search: %s found\n", name);
+					else
+						printf("Search: %s not found\n", name);
+					
+					break;
+
+				case 'd':
+					delete(name);
+					break;
+
+				default: { /* error */
+					fprintf(stderr, "Error: command to apply\n");
+					exit(EXIT_FAILURE);
+				}
+			} 
+    	}
+
+		else {
 			call_vector_unlock();
 			break;
 		}
-
-		const char* command = removeCommand();
-
-		/* unlock acess to call vector */
-		call_vector_unlock();
-
-		if (command == NULL) {
-			continue;
-		}
-
-		char token;
-		char name[MAX_INPUT_SIZE], type[MAX_INPUT_SIZE];
-		int numTokens = sscanf(command, "%c %s %s", &token, name, type);
-
-		if (numTokens < 2) {
-			fprintf(stderr, "Error: invalid command in Queue\n");
-			exit(EXIT_FAILURE);
-		}
-
-		int searchResult;
-		switch (token) {
-			case 'c':
-				switch (type[0]) {
-					case 'f':
-						printf("Create file: %s\n", name);
-						create(name, T_FILE);
-						break;
-					case 'd':
-						printf("Create directory: %s\n", name);
-						create(name, T_DIRECTORY);
-						break;
-					default:
-						fprintf(stderr, "Error: invalid node type\n");
-						exit(EXIT_FAILURE);
-				}
-				break;
-
-			case 'l': 
-				searchResult = lookup(name);
-				if (searchResult >= 0)
-					printf("Search: %s found\n", name);
-				else
-					printf("Search: %s not found\n", name);
-				
-				break;
-
-			case 'd':
-				printf("Delete: %s\n", name);
-				delete(name);
-				break;
-			
-			case 'm':
-				printf("Moving: %s to %s\n", name, type);
-				move(name, type);
-				break;			
-
-			default: { /* error */
-				fprintf(stderr, "Error: command to apply\n");
-				exit(EXIT_FAILURE);
-			}
-		}	
 	}
+}
+
+/* wrapper function, calling applyCommands */
+void* fnThread() {
+	applyCommands();
 	return NULL;
 }
 
 /* process pool initializer and runner */
 void processPool() {
-    int i;
+	int i = 0;
 	pthread_t tid[numberThreads];
-   	 
+   	
     for (i = 0; i < numberThreads; i++) {
-        if (pthread_create(&tid[i], NULL, applyCommands, NULL)) {
+        if (pthread_create(&tid[i], NULL, fnThread, NULL)) {
             fprintf(stderr, "Error: could not create threads\n");
             exit(EXIT_FAILURE);
         }
@@ -237,7 +232,6 @@ void processPool() {
     for (i = 0; i < numberThreads; i++) {
 		if (pthread_join(tid[i], NULL)) {
 			fprintf(stderr, "Error: could not join thread\n");
-			exit(EXIT_FAILURE);
 		}
     }
 
