11c7e3621309ce8ceb76b6116554862c848a3581
Sofia-Morgado <Sofia-Morgado@github.com>
2020-11-14 11:53:50 +0000

    changing printfs

---
+++ cmake-build-debug/CMakeFiles/TecnicoFS.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sofia/Desktop/TecnicoFS/fs/operations.c" "/home/sofia/Desktop/TecnicoFS/cmake-build-debug/CMakeFiles/TecnicoFS.dir/fs/operations.c.o"
+  "/home/sofia/Desktop/TecnicoFS/fs/state.c" "/home/sofia/Desktop/TecnicoFS/cmake-build-debug/CMakeFiles/TecnicoFS.dir/fs/state.c.o"
+  "/home/sofia/Desktop/TecnicoFS/main.c" "/home/sofia/Desktop/TecnicoFS/cmake-build-debug/CMakeFiles/TecnicoFS.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../fs"
+  "../inputs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
